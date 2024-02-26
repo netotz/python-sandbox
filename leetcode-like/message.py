e41066966fe742de1718f53141438cf83f8b7592
netotz <neto.otz@hotmail.com>
2024-02-25 18:20:14 -0600

    Refactor messages solution

--- leetcode-like/message.py
+++ leetcode-like/message.py
@@ -7,7 +7,6 @@ And now ...
 There is an animal with four legs
 """
 
-from ctypes.wintypes import LARGE_INTEGER
 import pytest
 
 
@@ -18,32 +17,32 @@ def crop_message(message: str, max_length: int) -> str:
     words = []
 
     first = 0
+    current_sum = 4
+    last_inserted = -1
 
     # O(n)
     for i, c in enumerate(message):
-        if c == " ":
-            words.append((first, len(message[first:i])))
-            first = i + 1
+        if c != " ":
+            continue
 
-    current_sum = 4
-    last_word_pos = 0
+        word_len = len(message[first:i])
+        words.append((first, word_len))
+        first = i + 1
+        last_inserted += 1
 
-    # O(n)
-    for i, (f, l) in enumerate(words):
-        current_sum += l + (0 if f == 0 else 1)
+        current_sum += word_len + (0 if last_inserted == 0 else 1)
 
         if current_sum > max_length:
-            last_word_pos = i - 1
             break
 
-    if last_word_pos < 0:
+    last_inserted -= 1
+
+    if last_inserted < 0:
         return "..."
 
-    last_word_start, last_word_len = words[last_word_pos]
+    last_word_start, last_word_len = words[last_inserted]
     last_char_pos = last_word_start + last_word_len - 1
     substring = message[: last_char_pos + 1]
-
-    print(words)
 
     return substring + " ..."
 
